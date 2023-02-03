d6e7c0072c4197892b3ac8cea99d514228a5aba2
Denis Mironov <denis-mironov1199@yandex.ru>
2023-02-03 15:25:19 +0300

    Update fill_database.py

--- api_yamdb/api/management/commands/fill_database.py
+++ api_yamdb/api/management/commands/fill_database.py
@@ -16,21 +16,7 @@ class Command(BaseCommand):
 
     help = 'Загрузка информации из csv файлов в базу данных'
 
-    # def fill_user(self):
-    #     with open(os.path.join(settings.BASE_DIR, 'static/data/users.csv'),
-    #               'rt', encoding='utf-8') as csv_file:
-    #         reader = csv.reader(csv_file)
-    #         for row in reader:
-    #             if row[0] != 'id':
-    #                 User.objects.get_or_create(
-    #                     id=row[0],
-    #                     username=row[1],
-    #                     email=row[2],
-    #                     role=row[3],
-    #                     bio=row[4],
-    #                     first_name=row[5],
-    #                     last_name=row[6]
-    #                 )
+
 
     def fill_category(self):
         with open(os.path.join(settings.BASE_DIR, 'static/data/category.csv'),
@@ -75,40 +61,12 @@ class Command(BaseCommand):
                         id=row[0], title_id=row[1], genres_id=row[2]
                     )
 
-    # def fill_review(self):
-    #     with open(os.path.join(settings.BASE_DIR, 'static/data/review.csv'),
-    #               'r', encoding='utf-8') as csv_file:
-    #         reader = csv.reader(csv_file)
-    #         for row in reader:
-    #             if row[0] != 'id':
-    #                 Review.objects.get_or_create(
-    #                     id=row[0],
-    #                     title_id=row[1],
-    #                     text=row[2],
-    #                     author_id=row[3],
-    #                     score=row[4],
-    #                     pub_date=row[5]
-    #                 )
-    #
-    # def fill_comments(self):
-    #     with open(os.path.join(settings.BASE_DIR, 'static/data/comments.csv'),
-    #               'r', encoding='utf-8') as csv_file:
-    #         reader = csv.reader(csv_file)
-    #         for row in reader:
-    #             if row[0] != 'id':
-    #                 Comment.objects.get_or_create(
-    #                     id=row[0],
-    #                     review_id=row[1],
-    #                     text=row[2],
-    #                     author_id=row[3],
-    #                     pub_date=row[4]
-    #                 )
+
 
     def handle(self, *args, **options):
-        # self.fill_user()
+
         self.fill_category()
         self.fill_genre()
         self.fill_titles()
         self.fill_genre_title()
-        # self.fill_review()
-        # self.fill_comments()
+
