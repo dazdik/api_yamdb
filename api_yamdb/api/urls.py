c93fa672a44bb42615dd47961397ebfc84ddfb17
Denis Mironov <denis-mironov1199@yandex.ru>
2023-02-05 22:42:55 +0300

    # first check files
    ---

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from .views import TitleViewSet, UserViewSet, get_token, signup
+from .views import TitleViewSet, CategoryViewSet, GenreViewSet
 
 router = DefaultRouter()
-router.register('title', TitleViewSet)
 router.register('users', UserViewSet, basename='users')
-
+router.register('titles', TitleViewSet)
+router.register('categories', CategoryViewSet)
+router.register('genres', GenreViewSet)
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/auth/signup/', signup, name='signup'),
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,10 +1,11 @@
 import uuid
 
+import django_filters
 from django.conf import settings
 from django.core.mail import send_mail
-from django.db import utils
 from django.shortcuts import get_object_or_404, render
-from rest_framework import filters, status, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, status, viewsets, mixins
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import (AllowAny, IsAuthenticated,
@@ -12,7 +13,7 @@ from rest_framework.permissions import (AllowAny, IsAuthenticated,
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .models import Title
+from .models import Title, Genre, Category
 from .permissions import (IsAdmin, IsAdminOrReadOnly,
                           IsAdminModeratorOwnerOrReadOnly)
 from .serializers import (
@@ -20,19 +21,66 @@ from .serializers import (
     SignUpSerializer,
     TitleSerializer,
     UserSerializer,
-    UserRoleSerializer
+    UserRoleSerializer, GenresSerializer, CategorySerializer,
+    TitleSerializerRead, TitleSerializerCreate
 )
 from users.models import User
 
 
 #
 # Create your views here.
+class CreateDestroyViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    pass
+
+
+class TitleFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(
+        field_name='name', lookup_expr='icontains'
+    )
+    year = django_filters.NumberFilter(field_name='year')
+    genre = django_filters.CharFilter(field_name='genre__slug')
+    category = django_filters.CharFilter(field_name='category__slug')
+
+    class Meta:
+        model = Title
+        fields = ['name', 'year', 'genre', 'category']
+
+
 class TitleViewSet(viewsets.ModelViewSet):
-    """
-    Класс для работы с Post url запросами и передачей данных в PostSerializer
-    """
-    queryset = Title.objects.all()
-    serializer_class = TitleSerializer
+    """Вьюсет для работы с произведениями."""
+    queryset = Title.objects.all().order_by('name')
+    serializer_class = TitleSerializerCreate
+    pagination_class = PageNumberPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.request.method in ('POST', 'PATCH', 'DELETE',):
+            return TitleSerializerCreate
+        return TitleSerializerRead
+
+
+class CategoryViewSet(CreateDestroyViewSet):
+    """Вьюсет для работы с категориями."""
+    queryset = Category.objects.all().order_by('name')
+    serializer_class = CategorySerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+    lookup_field = 'slug'
+
+
+class GenreViewSet(CreateDestroyViewSet):
+    """Вьюсет для работы с жанрами."""
+    queryset = Genre.objects.all().order_by('name')
+    serializer_class = GenresSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+    lookup_field = 'slug'
 
 
 class UserViewSet(viewsets.ModelViewSet):
