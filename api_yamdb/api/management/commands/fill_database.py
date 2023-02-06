f3da36486102bf2f89fa4ff95196789d0360a249
Denis Mironov <denis-mironov1199@yandex.ru>
2023-02-06 11:20:48 +0300

    # Fill database All models works
    all models works for filling database

--- api_yamdb/api/management/commands/fill_database.py
+++ api_yamdb/api/management/commands/fill_database.py
@@ -5,10 +5,8 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from reviews.models import Category, Genre, Title, GenreToTitle
+from users.models import User
 from reviews.models import Comment, Review
-
-
-# from users.models import User
 
 
 class Command(BaseCommand):
@@ -16,6 +14,22 @@ class Command(BaseCommand):
      python manage.py fill_database """
 
     help = 'Загрузка информации из csv файлов в базу данных'
+
+    def fill_user(self):
+        with open(os.path.join(settings.BASE_DIR, 'static/data/users.csv'),
+                  'rt', encoding='utf-8') as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                if row[0] != 'id':
+                    User.objects.get_or_create(
+                        id=row[0],
+                        username=row[1],
+                        email=row[2],
+                        role=row[3],
+                        bio=row[4],
+                        first_name=row[5],
+                        last_name=row[6]
+                    )
 
     def fill_category(self):
         with open(os.path.join(settings.BASE_DIR, 'static/data/category.csv'),
@@ -75,23 +89,25 @@ class Command(BaseCommand):
                         pub_date=row[5]
                     )
 
-    # def fill_comments(self):
-    #     with open(os.path.join(settings.BASE_DIR, 'static/data/comments.csv'),
-    #               'r', encoding='utf-8') as csv_file:
-    #         reader = csv.reader(csv_file)
-    #         for row in reader:
-    #             if row[0] != 'id':
-    #                 Comment.objects.get_or_create(
-    #                     id=row[0],
-    #                     review_id=row[1],
-    #                     text=row[2],
-    #                     author_id=row[3],
-    #                     pub_date=row[4]
-    #                 )
+    def fill_comments(self):
+        with open(os.path.join(settings.BASE_DIR, 'static/data/comments.csv'),
+                  'r', encoding='utf-8') as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                if row[0] != 'id':
+                    Comment.objects.get_or_create(
+                        id=row[0],
+                        review_id=row[1],
+                        text=row[2],
+                        author_id=row[3],
+                        pub_date=row[4]
+                    )
 
     def handle(self, *args, **options):
+        self.fill_user()
         self.fill_category()
         self.fill_genre()
         self.fill_titles()
         self.fill_genre_title()
-
+        self.fill_review()
+        self.fill_comments()
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -105,13 +105,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ],
-    #
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    # ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 
     "DEFAULT_PAGINATION_CLASS":
         'rest_framework.pagination.PageNumberPagination',
